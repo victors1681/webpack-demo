020cf27301664d4bdfcc149199f17ed92eeb4a0d
Victor Santos <v.santos@itsoluclick.com>
2018-12-09 13:53:58 -0500

    resolve added

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,17 @@ module.exports = env => {
     output: {
       path: path.join(__dirname, ".", "myDistribution", "ui"),
       filename: "js/[name]".concat(".[chunkhash:8].js")
+    },
+    resolve: {
+      alias: {
+        view: path.resolve(__dirname, "./src/View"),
+        container: path.resolve(__dirname, "./src/Container")
+      },
+      extensions: [".js"],
+      modules: [
+        path.resolve(__dirname, "./src"),
+        path.resolve(__dirname, "./node_modules")
+      ]
     }
   };
   return config;
